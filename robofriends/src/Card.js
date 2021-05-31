49e60722cf0c4ec759185014e5b69b2043799449
itssumitshukla <itssumitshukla@gmail.com>
2021-05-30 19:57:47 -1000

    removed most of the ootb setup and test build using robohash api

---
+++ robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ()=> {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow'>
+      <img alt='robot' src='https://robohash.org/test?200x200' />
+      <div>
+      <h2>Sumit</h2>
+        <p>Test@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
