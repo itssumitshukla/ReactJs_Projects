e5a95b85de143b5eea12eba03f8d86230702f56e
itssumitshukla <itssumitshukla@gmail.com>
2021-05-31 16:13:49 -1000

    testing prop change on dom

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -12,16 +12,20 @@ class App extends Component {
     }
   }
   //On search function
-  onSearchChange(e){
-    console.log(e.target.value)
+  onSearchChange = (e) => {
+    this.setState({searchfield: e.target.value});
   }
 
   render(){
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+      });
+
     return(
       <div className='tc'>
         <h1>Robofriends</h1>
         <Searchbox searchChange={this.onSearchChange} />
-        <Cardlist robots={this.state.robots}/>  
+        <Cardlist robots={filteredRobots}/>  
       </div>
     );
   }
