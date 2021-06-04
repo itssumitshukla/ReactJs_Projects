06028ccff3315c6e44be8ddd3e4484b2bd7dfaee
itssumitshukla <itssumitshukla@gmail.com>
2021-06-03 20:21:56 -1000

    setup logo components

--- facerecognitionbrain/src/components/Logo/Logo.js
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import 'tachyons';
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
   return (
-    <nav style={{display: 'flex', justifyContent: 'flex-start'}}>
-      <p className='ma4 mt0'>Sign Out</p>
-    </nav>
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+        </div>
+      </Tilt>
+    </div>
   );
-
 }
 
 export default Logo;
