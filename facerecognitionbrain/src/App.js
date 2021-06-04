73dc35d590ba6a2a702d8529a14cf4b7f69115b6
itssumitshukla <itssumitshukla@gmail.com>
2021-06-03 20:09:20 -1000

    added few npm packages and set up logo component

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 import 'tachyons';
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-  {/* {      <Logo />
-        <ImageLinkForm />
-        <FaceRecognition />} */}
+        <Logo />
       </div>
     );
   }
---
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'tachyons';
+
+const Logo = () => {
+  return (
+    <nav style={{display: 'flex', justifyContent: 'flex-start'}}>
+      <p className='ma4 mt0'>Sign Out</p>
+    </nav>
+  );
+
+}
+
+export default Logo;
