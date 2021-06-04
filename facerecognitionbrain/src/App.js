be0b65bbf9a91bb8dcc3f47b3f854af6fb5e5289
itssumitshukla <itssumitshukla@gmail.com>
2021-06-03 19:27:59 -1000

    fixed package.json error and add tachyons for styling

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
 import './App.css';
+import 'tachyons';
 
 class App extends Component() {
   render() {
