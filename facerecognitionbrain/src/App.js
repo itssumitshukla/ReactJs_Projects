7f3c5abb48f39ee7db745e59855893c08ee26f1b
itssumitshukla <itssumitshukla@gmail.com>
2021-06-06 18:42:58 -1000

    fix response json

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -41,9 +41,17 @@ class App extends Component {
     const width = Number(image.width);
     const height = Number(image.height);
     return {
-
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   };
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box});
+  }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -60,7 +68,7 @@ class App extends Component {
         console.log(
           response.outputs[0].data.regions[0].region_info.bounding_box
         );
-        this.calculateFaceLocation(response);
+        this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((err) => console.log(err));
   };
