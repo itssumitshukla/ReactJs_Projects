db1bce6f3e1a824bc24eb03e9b14de7361226a70
itssumitshukla <itssumitshukla@gmail.com>
2021-06-08 13:54:46 -1000

    fixed routing parm issues on navigationa nd signin

--- facerecognitionbrain/src/components/navigation/Navigation.js
+++ facerecognitionbrain/src/components/navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = ({onRouteChange}) => {
   return (
     <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
       <p 
-      onClick={onRouteChange}
+      onClick={()=> onRouteChange('signin')}
       className='f3 link dim black underline pa3 pointer'>Sign Out</p>
     </nav>
   );
