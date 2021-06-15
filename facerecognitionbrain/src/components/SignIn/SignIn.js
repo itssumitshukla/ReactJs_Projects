45987c04ed08e867fbc99310d0fcca2ed88c96e9
itssumitshukla <itssumitshukla@gmail.com>
2021-06-14 16:23:56 -1000

    test signin route

--- facerecognitionbrain/src/components/SignIn/SignIn.js
+++ facerecognitionbrain/src/components/SignIn/SignIn.js
@@ -15,6 +15,11 @@ class SignIn extends React.Component {
   onPasswordChange = (event) =>{
     this.setState({signInPassword : event.target.value})
   }
+
+  onSubmitSignIN = () => {
+    console.log(this.state);
+    this.props.onRouteChange('home');
+  }
   
   render(){
     const { onRouteChange } = this.props;
@@ -26,16 +31,26 @@ class SignIn extends React.Component {
             <legend className="f2 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+              <input 
+              onChange = {this.onEmailChange} 
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+              type="email" 
+              name="email-address" 
+              id="email-address" />
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+              <input 
+              onChange = {this.onPasswordChange}
+              className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+              type="password" 
+              name="password" 
+              id="password" />
             </div>
           </fieldset>
           <div className="">
             <input 
-            onClick={()=> onRouteChange('home')}
+            onClick={this.onSubmitSignIN}
             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
             type="submit" 
             value="Sign in" />
