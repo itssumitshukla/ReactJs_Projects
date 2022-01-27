574401d384eaee970f9d5c939fc1573c29c0d573
itssumitshukla <itssumitshukla@gmail.com>
2022-01-26 21:26:56 -1000

    testing css in react

--- my-app/src/Pokecard.js
+++ my-app/src/Pokecard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Pokecard.css';
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
@@ -7,7 +8,7 @@ class Pokecard extends Component {
     return (
       <div className='Pokecard'>
           <h1>{this.props.name}</h1>
-          <img src={imgSrc}/>
+          <img src={imgSrc} alt={this.props.name}/>
       </div>
     )
   }
