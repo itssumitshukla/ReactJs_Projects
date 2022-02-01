2dd5cb46515e0ae293fff3c8a80db30472799738
itssumitshukla <itssumitshukla@gmail.com>
2022-01-31 18:45:19 -1000

    testing multiple game option

--- my-app/src/Pokecard.js
+++ my-app/src/Pokecard.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import './Pokecard.css'
  
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
- 
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+//const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let padToThree = (number) => {return number <= 999 ? `00${number}`.slice(-3) : number}
+
+
 class Pokecard extends Component {
   render() {
     console.log(this.props.data)
