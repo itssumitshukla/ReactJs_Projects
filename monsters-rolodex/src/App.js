499a0edaf19379db4fd5b48148cc45609dd45bf7
itssumitshukla <itssumitshukla@gmail.com>
2023-01-01 23:31:51 -1000

    testing constructors in react

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,14 +6,22 @@ import "./App.css";
 //Testing Class
 
 class App extends Component {
+  //Local State
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Sumit",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi, {this.state.name}</p>
+          <button>Change Name</button>
           <a
             className="App-link"
             href="https://reactjs.org"
