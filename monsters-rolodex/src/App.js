1a10c6911091ad4570880d1f1fc96468659b9007
itssumitshukla <itssumitshukla@gmail.com>
2023-01-02 09:54:02 -1000

    Testing setstate on react app

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -21,7 +21,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi, {this.state.name}</p>
-          <button>Change Name</button>
+          <button
+            onClick={() => {
+              this.setState({ name: "Sumit" });
+            }}
+          >
+            Change Name
+          </button>
           <a
             className="App-link"
             href="https://reactjs.org"
