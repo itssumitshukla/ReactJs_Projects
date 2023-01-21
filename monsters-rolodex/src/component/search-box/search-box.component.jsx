13db246335613a2b1b6972b2f27368a0f1745152
itssumitshukla <itssumitshukla@gmail.com>
2023-01-20 22:46:16 -1000

    fix search box component

--- monsters-rolodex/src/component/search-box/search-box.component.jsx
+++ monsters-rolodex/src/component/search-box/search-box.component.jsx
@@ -1,17 +1,12 @@
-import { Component } from "react";
 import "./search-box.styles.css";
 
-class Searcbox extends Component {
-  render() {
-    return (
-      <input
-        className="search-box"
-        type={`search-box ${this.props.className}`}
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const Searcbox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className="search-box"
+    type={`search-box ${className}`}
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 
 export default Searcbox;
