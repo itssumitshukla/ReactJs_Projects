510ec445e7c355d88a69b5da88ef9642d0a15524
itssumitshukla <itssumitshukla@gmail.com>
2023-01-20 22:44:09 -1000

    fix card component to functional component

--- monsters-rolodex/src/component/card/card.component.jsx
+++ monsters-rolodex/src/component/card/card.component.jsx
@@ -1,21 +1,17 @@
-import { Component } from "react";
-
 import "./card.style.css";
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2size=1800x180`}
-        ></img>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { name, email, id } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2size=1800x180`}
+      ></img>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
