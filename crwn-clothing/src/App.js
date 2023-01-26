365ea8e7d7338d10e6aa36501ace181434d76fcf
itssumitshukla <itssumitshukla@gmail.com>
2023-01-25 21:11:06 -1000

    optimizing categories shell

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,4 +1,26 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
   return (
     <div className="categories-container">
       <div className="category-container">
