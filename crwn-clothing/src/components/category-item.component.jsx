443fe64aeab4f2251b8c16b1d2c6595414e0acce
itssumitshukla <itssumitshukla@gmail.com>
2023-01-27 23:40:13 -1000

    fix category html issue

--- crwn-clothing/src/components/category-item.component.jsx
+++ crwn-clothing/src/components/category-item.component.jsx
@@ -1,7 +1,22 @@
 import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
-  return console.log("Test");
+  return (
+    <div>
+      <div className="category-container">
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryItem;
