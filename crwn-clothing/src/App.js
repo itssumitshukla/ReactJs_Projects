4b79fc04908c9add55fa4c895eb6449087c8eee2
itssumitshukla <itssumitshukla@gmail.com>
2023-01-27 23:33:49 -1000

    fix image issue by adding obj destructuring

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -32,7 +32,12 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({ title, id, imageUrl }) => (
         <div className="category-container">
-          <div className="background-image" />
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
