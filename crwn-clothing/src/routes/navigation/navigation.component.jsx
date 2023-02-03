bf51e069b755061de854608b2a7002fa2e9ec6c0
itssumitshukla <itssumitshukla@gmail.com>
2023-02-02 23:45:57 -1000

    setup scss and navigation component

---
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation"></div>
+    </Fragment>
+  );
+};
+
+export default Navigation;
