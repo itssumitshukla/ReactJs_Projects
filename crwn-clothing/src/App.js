da9864677ee6c7a319d033ec82a14acf05a159e1
itssumitshukla <itssumitshukla@gmail.com>
2023-02-03 23:44:26 -1000

    fix route error in app.js for navigation

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component.jsx";
+import Navigation from "./routes/navigation/navigation.component";
 
 const App = () => {
   return (
