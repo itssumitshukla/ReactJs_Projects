b5fe028b5d7fbe931b78c644eb6faa870276082a
itssumitshukla <itssumitshukla@gmail.com>
2023-02-05 13:21:43 -1000

    test rendering sign in from homepage

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component.jsx";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route index element={<Shop />} />
+        <Route index element={<SignIn />} />
       </Route>
     </Routes>
   );
