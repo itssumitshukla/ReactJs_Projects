5f285ff0463135c7deff7adf4ad37563da2f2a09
itssumitshukla <itssumitshukla@gmail.com>
2023-02-04 14:21:04 -1000

    fix navigation rendering in homepage

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,11 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component.jsx";
 import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />} />
-      <Route path="/" index element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
