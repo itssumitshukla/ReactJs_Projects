0bd88c8a02bcdf192297c3647dba2b7afc2f15f5
itssumitshukla <itssumitshukla@gmail.com>
2023-02-05 13:20:05 -1000

    setup signin jsx

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route index element={<Shop />} />
       </Route>
     </Routes>
   );
