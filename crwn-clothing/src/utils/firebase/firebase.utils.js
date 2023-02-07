8814d840b4a85229284ce2184ce70e561b11b0c9
itssumitshukla <itssumitshukla@gmail.com>
2023-02-06 20:30:00 -1000

    setting up google firebase api

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -1,1 +1,7 @@
 import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
