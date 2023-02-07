6a386e9da64967adf6899e767b729ff6dd86c11f
itssumitshukla <itssumitshukla@gmail.com>
2023-02-06 20:52:22 -1000

    testing google authentication

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,19 @@
+import { signInWithGooglePopoup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return (
-    <div>
-      <h1>Sign In Page</h1>
-    </div>
-  );
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopoup();
+    console.log(response);
+    // const googleSignIn = () => {
+    //   const { user } = signInWithGooglePopup();
+    //   console.log(user);
+  };
+  // return (
+  //   <div>
+  //     <h1>SignIn</h1>
+  //     <button onClick={googleSignIn}>google sign in</button>
+  //   </div>
+  // );
 };
 
 export default SignIn;
