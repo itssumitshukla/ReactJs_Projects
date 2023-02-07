3751732a6e92fcac2392acfc92d5f463702cbec7
itssumitshukla <itssumitshukla@gmail.com>
2023-02-06 21:00:41 -1000

    fix aysnc await for authentication

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -4,16 +4,15 @@ const SignIn = () => {
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopoup();
     console.log(response);
-    // const googleSignIn = () => {
-    //   const { user } = signInWithGooglePopup();
-    //   console.log(user);
+    // const userDocRef = await createUserDocumentFromAuth(user);
   };
-  // return (
-  //   <div>
-  //     <h1>SignIn</h1>
-  //     <button onClick={googleSignIn}>google sign in</button>
-  //   </div>
-  // );
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
