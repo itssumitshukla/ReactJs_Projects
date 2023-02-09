c6daba75b5f91509c969b19b766765ac83fb599c
itssumitshukla <itssumitshukla@gmail.com>
2023-02-09 12:12:45 -1000

    fix the google redirect signin error

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
-  signInWithRedirect,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
@@ -11,19 +15,29 @@ const SignIn = () => {
   };
 
   const redirectGoogleResult = async () => {
-    const response = await getRedirectResult(auth);
+    const response = await redirectGoogleResult();
     console.log(response);
   };
 
   useEffect(() => {
-    redirectGoogleResult();
+    const fetchData = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+        console.log("signInWithGoogleRedirect", userDocRef);
+      }
+    };
+
+    fetchData().catch(console.error);
   }, []);
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleUser}>Sign in with Google Redirect</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   );
 };
