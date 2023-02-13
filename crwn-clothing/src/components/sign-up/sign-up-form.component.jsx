df20adaa110721b12811b8c71d4d9bf8aaa09f18
itssumitshukla <itssumitshukla@gmail.com>
2023-02-13 12:43:17 -1000

    fix button component rendering issue

--- crwn-clothing/src/components/sign-up/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up/sign-up-form.component.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import FormInput from "../form-input/form-input.component";
 
 import "./sign-up-form.styles.scss";
 
@@ -92,7 +94,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
