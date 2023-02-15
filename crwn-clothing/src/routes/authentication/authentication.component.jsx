4ef5b9d074622764109fe4f8aeeee5abdd8c6489
itssumitshukla <itssumitshukla@gmail.com>
2023-02-15 11:22:57 -1000

    testing new authentication component

--- crwn-clothing/src/routes/authentication/authentication.component.jsx
+++ crwn-clothing/src/routes/authentication/authentication.component.jsx
@@ -1,46 +1,20 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
-
-import {
-  auth,
-  signInWithGooglePopup,
-  signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
-
 import SignUpForm from "../../components/sign-up/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
+import "./authentication.styles.scss";
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   const redirectGoogleResult = async () => {
     const response = await redirectGoogleResult();
     console.log(response);
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await getRedirectResult(auth);
-      if (response) {
-        const userDocRef = await createUserDocumentFromAuth(response.user);
-        console.log("signInWithGoogleRedirect", userDocRef);
-      }
-    };
-
-    fetchData().catch(console.error);
-  }, []);
-
+const Authentication = () => {
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
   );
 };
+
 
 export default Authentication;
