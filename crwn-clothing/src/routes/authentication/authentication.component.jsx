0a05c8f890ffdd2e47c7f52b6c912f589591aa93
itssumitshukla <itssumitshukla@gmail.com>
2023-02-15 11:25:21 -1000

    setup scss for auth

--- crwn-clothing/src/routes/authentication/authentication.component.jsx
+++ crwn-clothing/src/routes/authentication/authentication.component.jsx
@@ -2,19 +2,14 @@ import SignUpForm from "../../components/sign-up/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 import "./authentication.styles.scss";
-const Authentication = () => {
-  const redirectGoogleResult = async () => {
-    const response = await redirectGoogleResult();
-    console.log(response);
-  };
+
 const Authentication = () => {
   return (
-    <div className='authentication-container'>
+    <div className="authentication-container">
       <SignInForm />
       <SignUpForm />
     </div>
   );
 };
-
 
 export default Authentication;
