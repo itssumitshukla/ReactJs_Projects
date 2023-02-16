4282fed43060e061c84c350d48319d546cb57ab4
itssumitshukla <itssumitshukla@gmail.com>
2023-02-16 12:18:49 -1000

    add userprovider to get provider value

--- crwn-clothing/src/contexts/user.context.jsx
+++ crwn-clothing/src/contexts/user.context.jsx
@@ -5,3 +5,11 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
 });
+
+//Actual component
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
