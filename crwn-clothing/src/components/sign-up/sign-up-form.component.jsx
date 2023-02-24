c359d0b34216f794f8dd21a49d4512fdbb4ca089
itssumitshukla <itssumitshukla@gmail.com>
2023-02-23 21:03:36 -1000

    fix dyanmic product component and hook it to app.js

--- crwn-clothing/src/components/sign-up/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up/sign-up-form.component.jsx
@@ -7,8 +7,6 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
-import { UserContext } from "../../contexts/user.context";
-
 import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
@@ -21,9 +19,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
-
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
--- crwn-clothing/src/contexts/products.context.jsx
+++ crwn-clothing/src/contexts/products.context.jsx
@@ -2,13 +2,17 @@ import { createContext, useState } from "react";
 import { Value } from "sass";
 import PRODUCTS from "../shop-data.json";
 
-export const ProductContext = createContext({
-    products: [],
+export const ProductsContext = createContext({
+  products: [],
 });
 
-export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState();
-    return{
-        <ProductContext.Provider value={}> {children} </ProductContext.Provider>
-    }
-}
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState();
+  const value = { products };
+  return (
+    <ProductsContext.Provider value={value}>
+      {" "}
+      {children}{" "}
+    </ProductsContext.Provider>
+  );
+};
