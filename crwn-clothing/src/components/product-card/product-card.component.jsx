148f586ec4724ecfcb936ef05ff5c999606df642
itssumitshukla <itssumitshukla@gmail.com>
2023-02-28 23:58:06 -1000

    fix price issue on product card

--- crwn-clothing/src/components/product-card/product-card.component.jsx
+++ crwn-clothing/src/components/product-card/product-card.component.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">{prrice}</span>
       </div>
       <Button buttonType="inverted">Add to card</Button>
     </div>
