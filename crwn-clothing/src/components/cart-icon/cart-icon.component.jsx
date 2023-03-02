85d49b92b04fa9aa2b2d7db61fc01c23782c5067
itssumitshukla <itssumitshukla@gmail.com>
2023-03-01 23:40:26 -1000

    setup carticon div

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,12 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  return console.log("Test");
+  return (
+    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
 };
 
 export default CartIcon;
