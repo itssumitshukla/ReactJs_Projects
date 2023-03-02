d225917c1a641a2fa62c60d7b214100e00c20d65
itssumitshukla <itssumitshukla@gmail.com>
2023-03-01 23:37:30 -1000

    setup cart icon component

---
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+
+import "./cart-icon.styles.scss";
+
+const CartIcon = () => {
+  return console.log("Test");
+};
+
+export default CartIcon;
