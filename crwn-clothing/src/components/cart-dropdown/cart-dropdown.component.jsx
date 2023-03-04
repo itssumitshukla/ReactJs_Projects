e2fd798a84db6d67ec7d2e0ee6151d05d48f0c4e
itssumitshukla <itssumitshukla@gmail.com>
2023-03-03 22:57:41 -1000

    return cart item in cart dropdown jsx

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,21 @@
+import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-  return console.log("Test");
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
