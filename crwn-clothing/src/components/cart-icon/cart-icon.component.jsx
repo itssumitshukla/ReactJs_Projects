58ffdbbe6538da23d0caef8f65b1899f6e2d5aa6
itssumitshukla <itssumitshukla@gmail.com>
2023-03-07 20:54:27 -1000

    set toggle function for cart icon

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -2,9 +2,13 @@ import { useContext } from "react";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
+import { CartContext } from "../../contexts/cart.context";
+
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container">
       <ShoppingIcon className="shopping-icon" />
