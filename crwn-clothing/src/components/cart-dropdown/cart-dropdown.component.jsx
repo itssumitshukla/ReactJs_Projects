464d1172872fb101c38b75603a20a5564983a0db
itssumitshukla <itssumitshukla@gmail.com>
2023-03-10 23:58:23 -1000

    fix card dropdown container loop

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import React from "react";
 
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 
 import CartItem from "../cart-item/cart-item.component";
@@ -13,9 +14,13 @@ const CartDropdown = () => {
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        cartItems.length ? ( cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} cartItem={cartItem} />
-        )) )
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
