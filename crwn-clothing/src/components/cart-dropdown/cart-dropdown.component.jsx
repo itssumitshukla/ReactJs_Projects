72187f16ab9095648c0b5f40716d7b2119ddca9f
itssumitshukla <itssumitshukla@gmail.com>
2023-03-10 23:57:18 -1000

    testing cart content using card dropdown

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
+import { useContext } from "react";
 import React from "react";
 
 import Button from "../button/button.component";
 
+import CartItem from "../cart-item/cart-item.component";
+
 import "./cart-dropdown.scss";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items" />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        cartItems.length ? ( cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} cartItem={cartItem} />
+        )) )
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
