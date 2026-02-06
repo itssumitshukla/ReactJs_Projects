f7e663c2f2fc09fe8f49ecf4e772ba19cf09bb63
itssumitshukla <itssumitshukla@gmail.com>
2026-02-05 14:00:13 -1000

    testing component test

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,4 +1,6 @@
 import logo from "./logo.svg";
+import Contact from "./component/Contact";
+
 import "./App.css";
 
 function App() {
@@ -9,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test React
-        </a>
+        <h1>
+          <Contact />
+        </h1>
       </header>
     </div>
   );
