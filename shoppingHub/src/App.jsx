09f595f79095e72a5fb32f39d20f087906b25f1d
itssumitshukla <itssumitshukla@gmail.com>
2026-02-08 18:13:08 -1000

    setup browerrouter and update app.jsx to render routes

--- shoppingHub/src/App.jsx
+++ shoppingHub/src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+
 import "./App.css";
+import AuthProvider from "./context/AuthContext";
+import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 function App() {
-  const name = "KONA";
-  const age = 5;
-
-  return <h1>Hello {name}</h1>;
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
