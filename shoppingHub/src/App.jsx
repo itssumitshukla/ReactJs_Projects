0e6c648f9f423bf008955b918742631decd5fe93
itssumitshukla <itssumitshukla@gmail.com>
2026-02-10 16:35:50 -1000

    fix render file

--- shoppingHub/src/App.jsx
+++ shoppingHub/src/App.jsx
@@ -2,28 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 
 import "./App.css";
-import AuthProvider from "./context/AuthContext";
-import ProductDetails from "./pages/ProductDetails";
-import CartProvider from "./context/CartContext";
+// import AuthProvider from "./context/AuthContext";
+// import ProductDetails from "./pages/ProductDetails";
+// import CartProvider from "./context/CartContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/products/:id" element={<ProductDetails />} />
-          </Routes>
-        </div>
-      </CartProvider>
-    </AuthProvider>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
   );
 }
 
