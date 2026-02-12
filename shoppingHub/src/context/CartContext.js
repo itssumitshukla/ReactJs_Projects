9bfeaf8d7d6e28c90e4ee893653708211cf5d324
itssumitshukla <itssumitshukla@gmail.com>
2026-02-12 12:14:05 -1000

    setup cart context js

---
+++ shoppingHub/src/context/CartContext.js
@@ -0,0 +1,8 @@
+import { createContext, useState, useContext } from "react";
+import { getProductById } from "../data/products";
+
+const CartContext = createContext(null);
+
+export default function CartProvider({ children }) {
+  <div>Test</div>;
+}
