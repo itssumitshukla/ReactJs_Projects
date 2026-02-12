da9e33156d4d0fcedaaf0298138b2ff984bb48b0
itssumitshukla <itssumitshukla@gmail.com>
2026-02-11 17:41:42 -1000

    setup user state for both email and password

--- shoppingHub/src/context/AuthContext.js
+++ shoppingHub/src/context/AuthContext.js
@@ -8,6 +8,22 @@ export default function AuthProvider({ children }) {
       ? { email: localStorage.getItem("currentUserEmail") }
       : null,
   );
+
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exists" };
+    }
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
+
+    setUser({ email });
+
+    return { success: true };
+  }
 }
 
 export function useAuth() {
