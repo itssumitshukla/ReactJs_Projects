4e2a08cd94f7b841a8cad898aa9f1e8cdaafe907
Sumit Shukla <itssumitshukla@gmail.com>
2026-08-25 16:29:18 -1000

    fix default export error

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -54,9 +54,9 @@ import './App.css'
       function ChatMessage({ message, sender }) {
         return (
           <div>
-            {sender === "robot" && <img src="robot.png" width="50" />}
+            {sender === "robot" && <img src={RobotProfileImage} width="50" />}
             {message}
-            {sender === "user" && <img src="user.png" width="50" />}
+            {sender === "user" && <img src={UserProfileImage} width="50" />}
           </div>
         );
       }
