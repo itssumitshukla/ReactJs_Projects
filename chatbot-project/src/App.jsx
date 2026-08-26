8da8140ed3f8d2ac243bc43bcb26cd183ca4613e
Sumit Shukla <itssumitshukla@gmail.com>
2026-08-25 16:36:14 -1000

    setup new chatInput component

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -4,52 +4,6 @@ import RobotProfileImage from './assets/react.svg'
 import UserProfileImage from './assets/user.png'
 import './App.css'
 
-      function ChatInput({ chatMessages, setChatMessages }) {
-        const [inputText, setInputText] = useState("");
-
-        function saveInputText(event) {
-          setInputText(event.target.value);
-        }
-
-        function sendMessage() {
-          const newChatMessages = [
-            ...chatMessages,
-            {
-              message: inputText,
-              sender: "user",
-              id: crypto.randomUUID(),
-            },
-          ];
-
-          setChatMessages(newChatMessages);
-
-          const response = Chatbot.getResponse(inputText);
-          setChatMessages([
-            ...newChatMessages,
-            {
-              message: response,
-              sender: "robot",
-              id: crypto.randomUUID().value,
-            },
-          ]);
-
-          setInputText("");
-        }
-
-        return (
-          <>
-            <input
-              placeholder="Send a message to Chatbot"
-              size="30"
-              onChange={saveInputText}
-              value={inputText}
-            />
-            <button onClick={sendMessage} className="send-button">
-              Send
-            </button>
-          </>
-        );
-      }
 
       function ChatMessage({ message, sender }) {
         return (
