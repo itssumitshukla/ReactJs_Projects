9a408eec5262365c2f0db571348669057f76f88a
Sumit Shukla <itssumitshukla@gmail.com>
2026-08-30 19:50:42 -1000

    fix chat message jsx error

--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,13 +1,24 @@
-import RobotProfileImage from '../assets/react.svg'
-import UserProfileImage from '../assets/user.png'
+import RobotProfileImage from '../assets/robot.png';
+import UserProfileImage from '../assets/user.png';
 import './ChatMessage.css';
 
-  export function ChatMessage({ message, sender }) {
-        return (
-          <div>
-            {sender === "robot" && <img src={RobotProfileImage} width="50" />}
-            {message}
-            {sender === "user" && <img src={UserProfileImage} width="50" />}
-          </div>
-        );
-      }
+export default function ChatMessage({ message, sender }) {
+
+  return (
+    <div className={
+      sender === 'user'
+        ? 'chat-message-user'
+        : 'chat-message-robot'
+    }>
+      {sender === 'robot' && (
+        <img src={RobotProfileImage} className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">
+        {message}
+      </div>
+      {sender === 'user' && (
+        <img src={UserProfileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
