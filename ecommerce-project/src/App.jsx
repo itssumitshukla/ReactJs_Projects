82bfeedb1cb6f97d32f6fb772bfb7cd2092bbf51
Sumit Shukla <itssumitshukla@gmail.com>
2026-09-15 22:21:15 -1000

    setuo react router with path import

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router';
 import { Homepage } from'./Pages/HomePage';
 import './App.css'
 
 function App() {
    return (
-
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
   )
 }
 
