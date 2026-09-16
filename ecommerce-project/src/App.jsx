f3c293d35800f595efa4a722b8f8bebe41717297
Sumit Shukla <itssumitshukla@gmail.com>
2026-09-15 22:33:17 -1000

    setup checkout element error handling

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router';
 import { Homepage } from'./Pages/HomePage';
+import { CheckoutPage } from'./Pages/CheckoutPage';
 import './App.css'
 
 function App() {
    return (
     <Routes>
       <Route path='/' element={<Homepage />} />
-      <Route path='checkout' element={CheckoutPage /} />
+      <Route path='checkout' element={<CheckoutPage />} />
     </Routes>
   )
 }
