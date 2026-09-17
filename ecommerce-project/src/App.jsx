1f63d737a740ee6a8efebfdd1d4344da7207a9ca
Sumit Shukla <itssumitshukla@gmail.com>
2026-09-16 17:39:42 -1000

    setup css

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
-import './App.css'
+import '../src/App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
-import '../Pages/HomePage.css';
+import './HomePage.css';
 
 export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
