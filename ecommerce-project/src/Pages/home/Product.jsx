5ee7db1ff99768f922229bbc028f845c7d01b4c4
Sumit Shukla <itssumitshukla@gmail.com>
2026-09-19 23:58:08 -1000

    setup product detail

--- ecommerce-project/src/Pages/home/Product.jsx
+++ ecommerce-project/src/Pages/home/Product.jsx
@@ -26,7 +26,7 @@ export function Product({ product, loadCart }) {
       </div>
 
       <div className="product-name limit-text-to-2-lines">
-        {product.name}
+        {product.name.detail}
       </div>
 
       <div className="product-rating-container">
