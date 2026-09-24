b329bfaa7fc871eaec84961e1a045fa064fe35a0
Sumit Shukla <itssumitshukla@gmail.com>
2026-09-23 18:27:04 -1000

    setup payment summary and order summary import

--- ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/Pages/checkout/CheckoutPage.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { OrderSummary } from './OrderSummary';
+import { PaymentSummary } from './PaymentSummary';
 import './checkout-header.css';
 import './CheckoutPage.css';
 
